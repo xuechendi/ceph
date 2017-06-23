bfea680f47b2d014f6dddf1345d4b0e7140a318f
Chendi.Xue <chendi.xue@intel.com>
2017-06-23 09:43:00 +0800

    Enable Multithreading Support
    Passed randread and randwrite test with zipf
    
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- src/librbd/cache/BlockGuard.cc
+++ src/librbd/cache/BlockGuard.cc
@@ -14,16 +14,21 @@
 namespace librbd {
 namespace cache {
 
-BlockGuard::BlockGuard(CephContext *cct, uint32_t max_blocks,
+BlockGuard::BlockGuard(CephContext *cct, uint64_t cache_size,
                        uint32_t block_size)
-  : m_cct(cct), m_max_blocks(max_blocks), m_block_size(block_size),
+  : m_cct(cct), m_max_blocks(cache_size%block_size?cache_size/block_size+1:cache_size/block_size), m_block_size(block_size),
     m_lock("librbd::cache::BlockGuard::m_lock"),
-    m_detained_block_pool(max_blocks),
-    m_detained_blocks_buckets(max_blocks),
+    m_detained_block_pool(m_max_blocks),
+    m_detained_blocks_buckets(m_max_blocks),
     m_detained_blocks(BlockToDetainedBlocks::bucket_traits(
-      &m_detained_blocks_buckets.front(), max_blocks)) {
+      &m_detained_blocks_buckets.front(), m_max_blocks)) {
   for (auto &detained_block : m_detained_block_pool) {
     m_free_detained_blocks.push_back(detained_block);
+  }
+  for(auto block = 0; block < m_max_blocks; block++) {
+    //chendi: initiate universal BlockIOMap
+    Block* block_info = new Block(block);
+    m_Block_map.insert(std::make_pair(block, block_info));
   }
 }
 
@@ -32,10 +37,10 @@ void BlockGuard::create_block_ios(IOType io_type,
                                   BlockIOs *block_ios,
                                   C_BlockRequest *block_request) {
   typedef std::unordered_map<uint64_t, BlockIO> BlockToBlockIOs;
-
   //ldout(m_cct, 20) << "image_extents=" << image_extents << dendl;
 
   BlockToBlockIOs block_to_block_ios;
+  BlockToBlocksMap::iterator block_it;
   uint64_t buffer_offset = 0;
   for (auto &extent : image_extents) {
     uint64_t image_offset = extent.first;
@@ -48,7 +53,12 @@ void BlockGuard::create_block_ios(IOType io_type,
       uint32_t block_length = block_end_offset - block_offset;
 
       // TODO block extent merging(?)
+      // chendi: find block_io from map
+      block_it = m_Block_map.find(block);
+      assert(block_it != m_Block_map.end());
       auto &block_io = block_to_block_ios[block];
+      block_io.block_info = block_it->second;
+      
       block_io.extents.emplace_back(buffer_offset, block_offset, block_length);
 
       buffer_offset += block_length;
@@ -64,8 +74,8 @@ void BlockGuard::create_block_ios(IOType io_type,
       pair.second.extents.size() != 1 ||
       pair.second.extents.front().block_offset != 0 ||
       pair.second.extents.front().block_length != m_block_size);
-    pair.second.block_request = block_request;
     pair.second.block = pair.first;
+    pair.second.block_request = block_request;
     pair.second.tid = 0;
 
     ldout(m_cct, 20) << "block_io=[" << pair.second << "]" << dendl;
@@ -75,7 +85,7 @@ void BlockGuard::create_block_ios(IOType io_type,
 
 int BlockGuard::detain(uint64_t block, BlockIO *block_io) {
   Mutex::Locker locker(m_lock);
-  ldout(m_cct, 1) << "block=" << block << ", "
+  ldout(m_cct, 20) << "block=" << block << ", "
                    << "free_slots=" << m_free_detained_blocks.size() << dendl;
   assert(block_io == nullptr || block == block_io->block);
 
@@ -90,14 +100,14 @@ int BlockGuard::detain(uint64_t block, BlockIO *block_io) {
     }
 
     if (block_io != nullptr) {
-      detained_block->block_ios.emplace_back(std::move(*block_io));
+      detained_block->block_ios.emplace_back(*block_io);
     }
 
     // alert the caller that the IO was detained
     return 1;
   } else {
     if (m_free_detained_blocks.empty()) {
-      ldout(m_cct, 1) << "no free detained block cells" << dendl;
+      ldout(m_cct, 20) << "no free detained block cells" << dendl;
       return -ENOMEM;
     }
 
@@ -106,6 +116,9 @@ int BlockGuard::detain(uint64_t block, BlockIO *block_io) {
 
     detained_block->block = block;
     m_detained_blocks.insert(*detained_block);
+    if (block_io != nullptr) {
+      detained_block->block_ios.emplace_back(*block_io);
+    }
     return 0;
   }
 }
@@ -116,10 +129,10 @@ int BlockGuard::release(uint64_t block, BlockIOs *block_ios) {
   assert(detained_block_it != m_detained_blocks.end());
 
   auto &detained_block = *detained_block_it;
-  ldout(m_cct, 1) << "block=" << block << ", "
+  ldout(m_cct, 20) << "block=" << block << ", "
                    << "pending_ios="
                    << (detained_block.block_ios.empty() ?
-                        0 : detained_block.block_ios.size() - 1) << ", "
+                        0 : detained_block.block_ios.size()) << ", "
                    << "free_slots=" << m_free_detained_blocks.size() << dendl;
 
   if (!detained_block.block_ios.empty()) {
